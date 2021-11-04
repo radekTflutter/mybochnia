66470a33d206521f1c00c5b9dd3c3e96d4a42af8
radekTflutter <radekTflutter@gmail.com>
2021-11-05 00:02:17 +0100

    New splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,86 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:flutter/services.dart';
+import 'home_screen.dart';
 
-void main() {
-  runApp(const MaterialApp(
-    title: 'Navigation Basics',
-    home: FirstRoute(),
-  ));
-}
+void main() => runApp(MyBochnia());
 
-class FirstRoute extends StatelessWidget {
-  const FirstRoute({Key? key}) : super(key: key);
-
+class MyBochnia extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('First Route'),
+    return MaterialApp(
+      title: 'My Bochnia',
+      theme: ThemeData(
+        primaryColor: Colors.white,
       ),
-      body: Center(
-        child: ElevatedButton(
-          child: const Text('Open route'),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const SecondRoute()),
-            );
-          },
-        ),
-      ),
-    );
-  }
-}
-
-class SecondRoute extends StatelessWidget {
-  const SecondRoute({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Second Route"),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: new Container(
-              width: 1200.0, height: 1200.0, child: new PictureWidget()),
-        ),
-      ),
-    );
-  }
-}
-
-class PictureWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new DecoratedBox(
-      child: new Container(
-        padding: EdgeInsets.all(60.0),
-        child: new Text(
-          'My Bochnia',
-          textAlign: TextAlign.center,
-          style: GoogleFonts.markaziText(
-            textStyle: TextStyle(
-              color: Colors.white,
-              fontSize: 55.0,
-            ),
-          ),
-        ),
-      ),
-      position: DecorationPosition.background,
-      decoration: new BoxDecoration(
-        shape: BoxShape.rectangle,
-        color: null,
-        image: new DecorationImage(
-            image: AssetImage(
-              "lib/pic/imgbochnia1.png",
-            ),
-            fit: BoxFit.fitHeight),
+      home: FutureBuilder(
+        future: Future.delayed(Duration(seconds: 3)),
+        builder: (ctx, timer) => timer.connectionState == ConnectionState.done
+            ? Home_screen()
+            : Container(
+                color: Colors.white,
+                child: Image(
+                  image: AssetImage("lib/pic/100.png"),
+                ),
+              ),
       ),
     );
   }
